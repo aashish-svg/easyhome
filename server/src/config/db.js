45551ceaa10e502015cbae14ff0b359168ad9bdf
aashish-svg <aasoni.soni@gmail.com>
2026-04-07 12:16:18 +0530

    Enforce Neon connection string protocol

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -10,17 +10,11 @@ module.exports = {
 };*/
 
 const { Pool } = require('pg');
-require('dotenv').config();
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=require`;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    connectionString: connectionString
 });
 
 module.exports = {
