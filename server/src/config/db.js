d4e6e82d358a2ed7e6083e62cedd26fb92b0dd27
aashish-svg <aasoni.soni@gmail.com>
2026-04-07 11:59:45 +0530

    Fix SSL requirement and remove .env from tracking

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,26 @@
-const { Pool } = require('pg');
+/*const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};*/
+
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = {
